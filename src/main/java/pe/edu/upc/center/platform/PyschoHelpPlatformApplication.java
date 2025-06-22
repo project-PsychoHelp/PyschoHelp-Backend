2ea94253a1c1560c7d1922c20bd2db30af022e67
EricMOB-afk <U202315032@upc.edu.pe>
2025-06-22 13:24:41 -0500

    chore: initial commit

---
+++ src/main/java/pe/edu/upc/center/platform/PyschoHelpPlatformApplication.java
@@ -0,0 +1,19 @@
+package pe.edu.upc.center.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@SpringBootApplication
+@EnableJpaAuditing
+
+
+public class PyschoHelpPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyschoHelpPlatformApplication.class, args);
+
+	}
+
+}
